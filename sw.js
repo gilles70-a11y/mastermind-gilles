6630d89572c5f97864c0ff75ed0c6e5a23977f99
gilles70-a11y <gilles70@gmail.com>
2026-03-29 14:33:55 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'mastermind-gilles-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './MasterGilles.png'
+];
+
+// Installa il Service Worker e salva i file in cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Risponde con i file della cache quando sei offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
